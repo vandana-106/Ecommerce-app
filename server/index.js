4c7e981187317e208ed6e1ca804698f2d135e7b3
vandana-106 <247588459+vandana-106@users.noreply.github.com>
2025-12-06 21:29:26 +0530

    Hook up /api/auth routes in server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js"
+import authRoutes from "./routes/authRoutes.js"
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/products/",productRoutes);
+app.use("/api/auth",authRoutes);
 
 app.get("/",(req,res)=>
 {
