2c50560e302debcf3036455dee2ad15b14cc940e
vandana-106 <247588459+vandana-106@users.noreply.github.com>
2025-12-09 20:49:33 +0530

    Mountes order api route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js"
 import authRoutes from "./routes/authRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js"
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/products/",productRoutes);
 app.use("/api/auth",authRoutes);
+app.use("/api/orders",orderRoutes);
 
 app.get("/",(req,res)=>
 {
