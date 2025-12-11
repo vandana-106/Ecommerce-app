03b27ae04c5c3cf20364c979ab81f3182322efde
vandana-106 <247588459+vandana-106@users.noreply.github.com>
2025-12-11 19:18:17 +0530

    Protect checkout route - login required

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useContext} from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar.jsx'
 import Home from '../pages/Home.jsx'
@@ -6,15 +6,18 @@ import ProductDetails from '../pages/ProductDetails.jsx'
 import CartPage from '../pages/CartPage.jsx'
 import LoginPage from '../pages/LoginPage.jsx'
 import RegisterPage from '../pages/RegisterPage.jsx'
+import CheckOutPage from '../pages/CheckOutPage.jsx'
+import { AuthContext } from '../context/AuthContext.jsx'
 
 
 const App = () => {
+  const {token}=useContext(AuthContext);
   return (
     <>
     <Navbar/>
     <Routes>
 
-      
+      <Route path="/checkout" element={token ? <CheckOutPage/> : <LoginPage/>}/>
       <Route path="/" element={<Home />}/>
       <Route path="/product/:id" element={<ProductDetails />}/>
       <Route path="/cart" element={<CartPage/>}/>
