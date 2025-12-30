196b5624398dfd4f019219da0a15d5c6fbb6c909
vandana-106 <247588459+vandana-106@users.noreply.github.com>
2025-12-30 21:43:58 +0530

    Enhanced navbar with hover effects, badge, and sticky layout

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,34 +1,38 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext.jsx";
-
+import { CartContext } from "../context/CartContext.jsx";
+import "./css/Navbar.css";
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
+  const {cart}=useContext(CartContext);
   return (
-    <nav
-      style={{
-        padding: "10px",
-        borderBottom: "1px solid #ddd",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <Link to="/">Home</Link>
-      <Link to="/cart">Cart</Link>
+    
+    <nav className="navbar">
+      <h2>E-Shop</h2>
+      <div className="navbar-links">
+      
+      <Link to="/" className="navbar-link">Home</Link>
+      <Link to="/cart" className="navbar-link">Cart
+      {cart.length >0 && (
+        <span className="cart-badge">{cart.length}</span>
+      )}
+      </Link>
+      <Link to="/products" className="navbar-link" >Products</Link>
 
       {user ? (
         <>
-          <span>HI {user.name}</span>
-          <button onClick={logout}>Logout</button>
+          <span style={{color:"#ccc"}}>HI {user.name}</span>
+          <button onClick={logout} className="logout-btn" >Logout</button>
         </>
       ) : (
         <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          <Link to="/login" className="navbar-link">Login</Link>
+          <Link to="/register" className="navbar-link">Register</Link>
         </>
       )}
+      </div>
     </nav>
   );
 };
